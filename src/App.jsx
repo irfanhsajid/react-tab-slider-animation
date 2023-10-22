2b1ade78fca80e2132efc5bab328c91daeb26017
Irfanul Haque Sajid <irfanhsajid@gmail.com>
2023-10-22 09:30:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import ReactTab from './ReactTab'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 style={{textAlign:'center', color:'tomato'}}>Bismillahir Rahmanir Rahim</h1>
+      <ReactTab />
+    </>
+  )
+}
+
+export default App

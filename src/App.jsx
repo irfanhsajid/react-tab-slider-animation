ab20d88ddea66978f4760702f3613d2f7fcbf03e
Irfanul Haque Sajid <irfanhsajid@gmail.com>
2023-10-22 09:35:52 +0600

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ function App() {
 
   return (
     <>
-      <h1 style={{textAlign:'center', color:'tomato'}}>Bismillahir Rahmanir Rahim</h1>
+      <h1 style={{ textAlign: 'center', color: 'tomato' }}>Bismillahir Rahmanir Rahim</h1>
       <ReactTab />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, debitis.</p>
     </>
   )
 }
